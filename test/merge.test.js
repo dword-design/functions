1b791f553c96646e039b78a420fb711d831719c8
Sebastian Landwehr <info@dword-design.de>
2019-12-21 18:07:38 +0100

    add merge test

--- test/merge.test.js
+++ test/merge.test.js
@@ -6,4 +6,5 @@ export default () => {
   expect({ foo: 1 } |> merge({ foo: 2 })).toEqual({ foo: 2 })
   expect({ foo: 1 } |> merge({ foo: 2 }, { bar: 3 })).toEqual({ foo: 2, bar: 3 })
   expect({ foo: 1 } |> merge([{ bar: 2 }, { baz: 3 }])).toEqual({ foo: 1, 0: { bar: 2 }, 1: { baz: 3 } })
+  expect({ foo: { bar: 2 } } |> merge({ foo: { baz: 3 } })).toEqual({ foo: { bar: 2, baz: 3 } })
 }
