80318acb81a490648ae49927313de545d1d43989
Sebastian Landwehr <info@dword-design.de>
2019-12-28 19:25:28 +0100

    bump version

--- test/drop-right.test.js
+++ test/drop-right.test.js
@@ -1,7 +1,7 @@
-import { drop } from '@dword-design/functions'
+import { dropRight } from '@dword-design/functions'
 import expect from 'expect'
 
 export default () => {
-  expect([1, 2, 3] |> drop()).toEqual([1, 2])
-  expect([1, 2, 3] |> drop(2)).toEqual([1])
+  expect([1, 2, 3] |> dropRight()).toEqual([1, 2])
+  expect([1, 2, 3] |> dropRight(2)).toEqual([1])
 }
