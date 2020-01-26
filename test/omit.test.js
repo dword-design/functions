4c50d3d54958c88543d62475d1ff3c548feaefcd
Sebastian Landwehr <info@dword-design.de>
2020-01-26 10:35:47 +0100

    add test for omit

--- test/omit.test.js
+++ test/omit.test.js
@@ -1,4 +1,7 @@
 import { omit } from '@dword-design/functions'
 import expect from 'expect'
 
-export default () => expect({ foo: 1, bar: 2 } |> omit('foo')).toEqual({ bar: 2 })
+export default () => {
+  expect({ foo: 1, bar: 2 } |> omit('foo')).toEqual({ bar: 2 })
+  expect({ foo: 1, bar: 2, baz: 3 } |> omit(['foo', 'bar'])).toEqual({ baz: 3 })
+}
