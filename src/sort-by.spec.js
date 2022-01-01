2fb34b4fe178db27f0f9623855e937d789372bb3
dword <info@sebastianlandwehr.com>
2022-01-01 22:44:19 +0000

    chore: test for multiple compare functions

--- src/sort-by.spec.js
+++ src/sort-by.spec.js
@@ -7,6 +7,18 @@ export default {
       { foo: 1 },
       { foo: 2 },
     ]),
+  'multiple compare functions': () =>
+    expect(
+      [
+        { bar: 1, foo: 2 },
+        { bar: 2, foo: 1 },
+        { bar: 1, foo: 1 },
+      ] |> sortBy(['foo', 'bar'])
+    ).toEqual([
+      { bar: 1, foo: 1 },
+      { bar: 2, foo: 1 },
+      { bar: 1, foo: 2 },
+    ]),
   object: () =>
     expect({ 1: 'foo', 2: 'bar' } |> sortBy(identity)).toEqual(['bar', 'foo']),
 }
