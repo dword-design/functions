91382da30cc75604935727b6e26a519c362ad278
Sebastian Landwehr <info@sebastianlandwehr.com>
2025-04-13 11:50:58 +0000

    chore: linting

--- src/max-by.spec.js
+++ src/max-by.spec.js
@@ -3,5 +3,7 @@ import self from './max-by.js';
 export default {
   empty: () => expect([] |> self('foo')).toEqual(undefined),
   valid: () =>
-    expect([{ foo: 1 }, { foo: 6 }, { foo: 2 }] |> self('foo')).toEqual({ foo: 6 }),
+    expect([{ foo: 1 }, { foo: 6 }, { foo: 2 }] |> self('foo')).toEqual({
+      foo: 6,
+    }),
 };
--- src/min-by.spec.js
+++ src/min-by.spec.js
@@ -1,3 +1,8 @@
-import self from './minBy.js';
+import self from './min-by.js';
 
-export default { valid: () => expect([{ value: 2 }, { value: 1 }, { value: 3 }] |> self('value')).toEqual({ value: 1 }) };
+export default {
+  valid: () =>
+    expect([{ value: 2 }, { value: 1 }, { value: 3 }] |> self('value')).toEqual(
+      { value: 1 },
+    ),
+};
